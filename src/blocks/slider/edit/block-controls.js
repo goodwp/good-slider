63f9c8eb03382188f7b7536c086d2cfdb932cd64
Fabian Todt <mail@fabiantodt.at>
2024-03-14 20:03:00 +0100

    fix slide variations inserting

--- src/blocks/slider/edit/block-controls.js
+++ src/blocks/slider/edit/block-controls.js
@@ -2,6 +2,10 @@ import {
     BlockControls,
     store as blockEditorStore,
 } from "@wordpress/block-editor";
+import {
+    createBlocksFromInnerBlocksTemplate,
+    store as blocksStore,
+} from "@wordpress/blocks";
 import { ToolbarButton, ToolbarGroup } from "@wordpress/components";
 import { addCard as addSlideIcon } from "@wordpress/icons";
 import { __ } from "@wordpress/i18n";
@@ -19,9 +23,17 @@ import { useHasTemplateLock } from "./util";
 const SliderBlockControls = ( { clientId } ) => {
     const shouldRenderAppender = useHasTemplateLock( clientId );
     const { insertBlock } = useDispatch( blockEditorStore );
+    const defaultVariation = useSelect( ( select ) => {
+        const { getDefaultBlockVariation } = select( blocksStore );
+        return getDefaultBlockVariation( "good-slider/slide", "block" );
+    }, [] );
     // Create a Slide block and insert it.
     const addSlide = () => {
-        const block = createBlock( "good-slider/slide" );
+        const block = createBlock(
+            "good-slider/slide",
+            defaultVariation?.attributes,
+            createBlocksFromInnerBlocksTemplate( defaultVariation?.innerBlocks )
+        );
         insertBlock( block, undefined, clientId, true );
     };
     return (
